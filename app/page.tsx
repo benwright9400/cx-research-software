7c363600589f99c3e267dfaf03fa752904e4f418
Ben Wright <benwright9400@gmail.com>
2025-08-17 12:22:14 +0100

    Add Google 0Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
-import Image from "next/image";
+"use client"
+
 import Header from "./components/Header";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return <button onClick={() => signIn("google")}>Sign in with google</button>;
+  }
+
   return (
     <div className="">
       <Header />
+
+      <div>
+        <p>User: {session.user?.name}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
     </div>
   );
 }
