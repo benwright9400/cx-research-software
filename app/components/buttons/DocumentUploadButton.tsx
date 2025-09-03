c688e4db7dd2bee89165c375d11d4e2927d6fcb8
Ben Wright <benwright9400@gmail.com>
2025-09-03 17:14:34 +0100

    Add findings navigation

--- app/components/buttons/DocumentUploadButton.tsx
+++ app/components/buttons/DocumentUploadButton.tsx
@@ -8,7 +8,7 @@ export default function DocumentUploadButton() {
     return (
         <button
             type="button"
-            className="rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            className="ml-auto w-fit rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
             onClick={() => { popup.openPopup(); popup.setTitle("Document Management"); }}
         >
             Manage Documents
